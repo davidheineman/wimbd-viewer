7370e089eadeddec2c609d155780538dc031fbe2
davidheineman <dheineman3@gatech.edu>
2025-01-13 19:09:06 -0800

    make extension clickable

--- extension/background.js
+++ extension/background.js
@@ -11,16 +11,21 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "getWIMBDCount") {
     chrome.tabs.sendMessage(tab.id, { type: 'getSelectionCoords' }, (response) => {
-      // First, show the loading popup
+      let query = info.selectionText
+
+      const index_name = 'docs_v1.7_2024-06-04';
+      const seeMoreUrl = `http://localhost:3000/index.html?idx=${index_name}&q=${encodeURIComponent(query)}`;
+      const see_more = `(<a href="${seeMoreUrl}" target="_blank">see more</a>)`;
+      
       chrome.tabs.sendMessage(tab.id, {
         type: 'showResults',
-        results: '<div class="loading">Loading...</div>',
+        results: `<div class="loading">Loading... ${see_more}</div>`,
         x: response?.x,
         y: response?.y,
-        query: info.selectionText
+        query: query
       });
       
-      const selectedText = info.selectionText;
+      const selectedText = query;
       fetchCounts(selectedText, response, tab);
     });
   }
@@ -32,12 +37,13 @@ async function fetchCounts(query, coords, tab) {
     const index_name = 'docs_v1.7_2024-06-04';
     const seeMoreUrl = `http://localhost:3000/index.html?idx=${index_name}&q=${encodeURIComponent(query)}`;
     const response = await fetch(`http://localhost:3000/counts?index=${index_name}&q=${encodeURIComponent(query)}`);
+    const see_more = `(<a href="${seeMoreUrl}" target="_blank">see more</a>)`;
     const data = await response.json();
 
 
     chrome.tabs.sendMessage(tab.id, {
       type: 'showResults',
-      results: `${data.count} occurrences in Dolma 1.7 (<a href="${seeMoreUrl}" target="_blank">see more</a>)`,
+      results: `${data.count} occurrences in Dolma 1.7 ${see_more}`,
       query: query,
       x: coords?.x,
       y: coords?.y,
@@ -74,3 +80,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   });
 }); 
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({ url: 'http://localhost:3000' });
+});
